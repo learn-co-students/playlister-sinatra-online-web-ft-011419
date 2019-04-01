8f160421e8576639f4ffe597c88bca0f0bb61e81
Jared Harbison <jared.harbison@gmail.com>
2019-04-01 12:07:57 -0400

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,25 @@
 class ArtistsController < ApplicationController
+  get '/artists' do
+    @artists = Artist.all
+    erb :'/artists/index'
+  end
+
+  get '/artists/new' do
+    erb :'/artists/new'
+  end
+
+  get '/artists/:slug' do
+    @artist = Artist.find_by_slug(params[:slug])
+    erb :'/artists/show'
+  end
+
+  post '/artists' do
+    @artist = Artist.create(name: params["Name"])
+    @artist.song = Song.find_or_create_by(name: params["Song Name"])
+    @artist.genre_ids = params[:genres]
+    @artist.save
+
+    flash[:message] = "Successfully created artist."
+    redirect "/artists/#{@artist.slug}"
+  end
 end
---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,24 @@
+class GenresController < ApplicationController
+
+  get '/genres' do
+    @genres = Genre.all
+    erb :'/genres/index'
+  end
+
+  get '/genres/new' do
+    erb :'/genres/new'
+  end
+
+  get '/genres/:slug' do
+    @genre = Genre.find_by_slug(params[:slug])
+    erb :'/genres/show'
+  end
+
+  post '/genres' do
+    @genre = Genre.create(name: params["Name"])
+    @genre.song = Song.find_or_create_by(name: params["Artist Name"])
+    @genre.save
+    flash[:message] = "Successfully created genre."
+    redirect "/genres/#{@genre.slug}"
+  end
+end 
